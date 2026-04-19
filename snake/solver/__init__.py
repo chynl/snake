3c1747605d40aab9f256d7477553668e3b8a0e55
chynl <14769273+chynl@users.noreply.github.com>
2026-04-19 12:59:39 -0700

    squash commits

--- snake/solver/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""AI solvers package."""
-
-from snake.solver.path import PathSolver
-from snake.solver.greedy import GreedySolver
-from snake.solver.hamilton import HamiltonSolver
-from snake.solver.dqn import DQNSolver
